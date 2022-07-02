dd611bd8181c67dcf3b50de944f9ad3b0eb34dee
juliocnsouzadev <juliocnsouzadev@gmail.com>
2022-07-02 15:34:59 +0100

    add custom rule sample

--- simple-app/main.go
+++ simple-app/main.go
@@ -15,6 +15,34 @@ import (
 var (
 	appName = os.Getenv("APP_NAME")
 
+	numberOfSalesHistogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: "app_api",
+			Name:      "sales_number_histogram",
+			Help:      fmt.Sprintf("This is a sales number histogram for %s", appName),
+		})
+
+	valueOfSalesHistogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: "app_api",
+			Name:      "sales_value_histogram",
+			Help:      fmt.Sprintf("This is a sales value histogram for %s", appName),
+		})
+
+	numberOfExpendHistogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: "app_api",
+			Name:      "expend_number_histogram",
+			Help:      fmt.Sprintf("This is a expend number histogram for %s", appName),
+		})
+
+	valuesOfExpendHistogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: "app_api",
+			Name:      "expend_value_histogram",
+			Help:      fmt.Sprintf("This is a expend value histogram for %s", appName),
+		})
+
 	counter = prometheus.NewCounter(
 		prometheus.CounterOpts{
 			Namespace: "app_api",
@@ -56,6 +84,11 @@ func main() {
 
 	http.Handle("/metrics", newHandlerWithHistogram(promhttp.Handler(), histogramVec))
 
+	prometheus.MustRegister(numberOfSalesHistogram)
+	prometheus.MustRegister(valueOfSalesHistogram)
+	prometheus.MustRegister(numberOfExpendHistogram)
+	prometheus.MustRegister(valuesOfExpendHistogram)
+
 	prometheus.MustRegister(counter)
 	prometheus.MustRegister(gauge)
 	prometheus.MustRegister(histogram)
@@ -63,19 +96,38 @@ func main() {
 
 	go func() {
 
-		factor := float64(rand.Intn(10))
-		
 		for {
-			counter.Add(rand.Float64() * factor)
-			gauge.Add(rand.Float64()*factor - 5)
-			histogram.Observe(rand.Float64() * factor)
-			summary.Observe(rand.Float64() * factor)
+			updateRandomMetrics()
+			updateBusinessMetrics()
 
 			time.Sleep(time.Second * 2)
+
 		}
 	}()
 
 	log.Fatal(http.ListenAndServe(":2112", nil))
+}
+
+func updateRandomMetrics() {
+	counter.Add(rand.Float64() * 5)
+	gauge.Add(rand.Float64()*5 - 5)
+	histogram.Observe(rand.Float64() * 5)
+	summary.Observe(rand.Float64() * 5)
+}
+
+func updateBusinessMetrics() {
+	numberOfSales := float64(rand.Intn(10))
+	valuePerSale := rand.Float64()
+	totalSales := numberOfSales * valuePerSale
+	numberOfSalesHistogram.Observe(numberOfSales)
+	valueOfSalesHistogram.Observe(totalSales)
+
+	numberOfExpend := float64(rand.Intn(5))
+	valuePerExpend := valuePerSale * 0.65
+	totalExpend := numberOfExpend * valuePerExpend
+	numberOfExpendHistogram.Observe(numberOfExpend)
+	valuesOfExpendHistogram.Observe(totalExpend)
+
 }
 
 func newHandlerWithHistogram(handler http.Handler, histogram *prometheus.HistogramVec) http.Handler {
